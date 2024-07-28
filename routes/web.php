954244709919ae64ffd1ed903603276b6cc40b7f
Gregory Buzdyk <buzdyk.gregory@gmail.com>
2024-07-29 01:21:55 +0700

    remove livewire

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function() {
     return redirect()->to('/today');
 });
-Route::get('/month-review', \App\Livewire\MonthReview::class);
-Route::get('/wishlist', \App\Livewire\Wishlist::class);
 
 Route::get('/today', [\App\Http\Controllers\TodayController::class, 'index']);
 Route::get('/{year}/{month}', [\App\Http\Controllers\MonthController::class, 'index']);
